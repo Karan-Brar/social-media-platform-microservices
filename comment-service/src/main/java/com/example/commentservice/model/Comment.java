4b4e43128dd2748e040b64ee664685ebe6e60f7c
Karanvir Brar <brarkaran938@gmail.com>
2025-03-04 19:34:03 -0600

    Created the Comment Model

--- comment-service/src/main/java/com/example/commentservice/model/Comment.java
+++ comment-service/src/main/java/com/example/commentservice/model/Comment.java
@@ -1,4 +1,72 @@
 package com.example.commentservice.model;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "Documents")
 public class Comment {
+
+    @Id
+    private String id;
+
+    private String postId;
+
+    private String userId;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public void Comment() {}
+
+    public void Comment(String postId, String userId, String content){
+        this.postId = postId;
+        this.userId = userId;
+        this.content = content;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
