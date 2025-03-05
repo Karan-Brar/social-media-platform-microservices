a17c6b659b44c381e96092935ff90d5cb895a46a
Karanvir Brar <brarkaran938@gmail.com>
2025-03-04 19:38:08 -0600

    Created the Comment Repository

---
+++ comment-service/src/main/java/com/example/commentservice/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.example.commentservice.repository;
+
+import com.example.commentservice.model.Comment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, String> {
+    List<Comment> findByPostId(String postId); // Find all comments for a post
+    List<Comment> findByUserId(String userId); // Find all comments made by a specific user on different posts
+}
