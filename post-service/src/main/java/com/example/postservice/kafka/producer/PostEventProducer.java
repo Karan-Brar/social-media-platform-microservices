b527e5ae66b2e0eeb4045667354d3b27d2e9dc50
Karanvir Brar <brarkaran938@gmail.com>
2025-03-05 21:55:59 -0600

    Added event publishing on post creation

--- post-service/src/main/java/com/example/postservice/kafka/producer/PostEventProducer.java
+++ post-service/src/main/java/com/example/postservice/kafka/producer/PostEventProducer.java
@@ -1,5 +1,6 @@
 package com.example.postservice.kafka.producer;
 
+import com.example.postservice.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,14 @@ public class PostEventProducer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     private static final String POST_DELETED_TOPIC = "post-deleted";
+    private static final String POST_CREATED_TOPIC = "post-created";
 
     public void sendPostDeletedEvent(String postId){
         kafkaTemplate.send(POST_DELETED_TOPIC, postId);
+    }
+
+    public void sendPostCreatedEvent(Post post){
+        String message = post.getUserId() + ":" + post.getContent();
+        kafkaTemplate.send(POST_CREATED_TOPIC, message);
     }
 }
